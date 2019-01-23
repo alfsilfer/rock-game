bf6823ac91c678e8072e0b45eeb4c6a47fc401b8
Alfonso Silva <alfsilfer@gmail.com>
2019-01-23 19:32:41 +0100

    version3.0

--- js/game.js
+++ js/game.js
@@ -3,23 +3,23 @@ var Game = {
   ctx: undefined,
   fps: 60,
   frameCounter: 0,
-  // obstacles: [],
+  players : {},
 
   startGame: function(id) {
     this.background = new Background(this);
     this.player = new Player(this);
-    this.player2 = new Player2(this);
+    this.player.playerNumber = 1;
+    this.player2 = new Player(this);
+    this.player2.x = 1650;
+    this.player2.playerNumber = 2;
     this.obstacles = new Obstacles(this);
-
 
     this.canvas = document.getElementById(id);
     this.ctx = this.canvas.getContext("2d");
     // this.drawAll();
 
-    this.player.setListeners();
-    this.player.dontMove();
-    this.player2.setListeners2();
-    this.player2.dontMove();
+    this.setListeners();
+    this.dontMove();
     this.interval = setInterval(
       function() {
         this.frameCounter++;
@@ -55,13 +55,17 @@ var Game = {
     var cond1, cond2, cond3, cond4;
     for (i = 0; i < this.obstacles.arrObs.length; i++) {
       cond1 = this.player.x + this.player.width >= this.obstacles.arrObs[i].x;
-      cond2 = this.obstacles.arrObs[i].x + this.obstacles.arrObs[i].width >= this.player.x;
+      cond2 =
+        this.obstacles.arrObs[i].x + this.obstacles.arrObs[i].width >=
+        this.player.x;
       cond3 = this.player.y + this.player.height >= this.obstacles.arrObs[i].y;
-      cond4 = this.obstacles.arrObs[i].y + this.obstacles.arrObs[i].height >= this.player.y + this.player.height;
+      cond4 =
+        this.obstacles.arrObs[i].y + this.obstacles.arrObs[i].height >=
+        this.player.y + this.player.height;
 
       if (cond1 && cond2 && cond3 && cond4) {
         this.player.y = this.obstacles.arrObs[i].y - this.player.height;
-        this.player.speedY = 0
+        this.player.speedY = 0;
       }
     }
   },
@@ -69,15 +73,64 @@ var Game = {
     var cond1, cond2, cond3, cond4;
     for (i = 0; i < this.obstacles.arrObs.length; i++) {
       cond1 = this.player2.x + this.player2.width >= this.obstacles.arrObs[i].x;
-      cond2 = this.obstacles.arrObs[i].x + this.obstacles.arrObs[i].width >= this.player2.x;
-      cond3 = this.player2.y + this.player2.height >= this.obstacles.arrObs[i].y;
-      cond4 = this.obstacles.arrObs[i].y + this.obstacles.arrObs[i].height >= this.player2.y + this.player2.height;
+      cond2 =
+        this.obstacles.arrObs[i].x + this.obstacles.arrObs[i].width >=
+        this.player2.x;
+      cond3 =
+        this.player2.y + this.player2.height >= this.obstacles.arrObs[i].y;
+      cond4 =
+        this.obstacles.arrObs[i].y + this.obstacles.arrObs[i].height >=
+        this.player2.y + this.player2.height;
 
       if (cond1 && cond2 && cond3 && cond4) {
         this.player2.y = this.obstacles.arrObs[i].y - this.player2.height;
-        this.player2.speedY = 0
+        this.player2.speedY = 0;
       }
     }
+  },
+  setListeners: function() {
+    document.onkeydown = function(event) {
+      switch (event.keyCode) {
+        case 65:
+          this.player.moveLeft();
+          break;
+        case 68:
+          this.player.moveRight();
+          break;
+        case 87:
+          this.player.y -= 150;
+          break;
+        case 81:
+          this.player.punch();
+          break;
+        case 75:
+          this.player2.moveLeft();
+          break;
+        case 192:
+          this.player2.moveRight();
+          break;
+        case 79:
+          this.player2.y -= 150;
+          break;
+        case 73:
+          this.player2.punch();
+          break;
+      }
+    }.bind(this);
+  },
+  dontMove: function() {
+    document.onkeyup = function(event) {
+      switch (event.keyCode) {
+        case 68:
+        case 65:
+        case 75:
+        case 192:
+          // case 83:
+          // case 87:
+          this.player.noMove();
+          this.player2.noMove();
+          break;
+      }
+    }.bind(this);
   }
-  
 };
--- js/player.js
+++ js/player.js
@@ -10,6 +10,7 @@ function Player(game) {
   this.speedY = 0;
   this.limitLeft = true;
   this.limitRight = true;
+  this.playerNumber = 0;
 }
 Player.prototype.draw = function() {
   this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -55,42 +56,6 @@ Player.prototype.up = function() {
     // console.log("salta kanguro", this.y);
   }
 };
-Player.prototype.setListeners = function() {
-  document.onkeydown = function(event) {
-    switch (event.keyCode) {
-      case 65:
-        this.moveLeft();
-        break;
-      case 68:
-        this.moveRight();
-        break;
-      case 87:
-        this.y -= 150;
-        break;
-      case 81:
-        this.punch();
-        break;
-      // case 87:
-      //   this.moveUp();
-      //   break;
-      // case 83:
-      //   this.moveDown();
-      //   // break;
-    }
-  }.bind(this);
-};
-Player.prototype.dontMove = function() {
-  document.onkeyup = function(event) {
-    switch (event.keyCode) {
-      case 68:
-      case 65:
-        // case 83:
-        // case 87:
-        this.noMove();
-        break;
-    }
-  }.bind(this);
-};
 Player.prototype.punch = function() {
   this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
   // console.log("Punch!");
@@ -107,3 +72,4 @@ Player.prototype.boundary = function() {
     this.limitRight = true;
   }
 };
+
